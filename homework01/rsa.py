190310237404c1f9c0ac9de32bea178796910660
857l <61349357+857l@users.noreply.github.com>
2022-11-07 12:16:49 +0300

    Update rsa.py

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -26,6 +26,7 @@ def multiplicative_inverse(e: int, phi: int) -> int:
 
 def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
     import random
+    
     if not (is_prime(p) and is_prime(q)):
         raise ValueError("Both numbers must be prime.")
     elif p == q:
