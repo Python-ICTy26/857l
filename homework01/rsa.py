69a719af2b8677ccd6708959eb4f02b350aad299
857l <61349357+857l@users.noreply.github.com>
2022-11-07 13:19:20 +0300

    Update rsa.py

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -3,13 +3,14 @@ import typing as tp
 
 
 def is_prime(n: int) -> bool:
-    k = 0
-    for i in range(2, n):
+    if n <= 1:
+        return False
+    flag = True
+    for i in range(2, n // 2 + 1):
         if n % i == 0:
-            k = 1
-        if k > 0:
-            return False
-    return True
+            flag = False
+            break
+    return flag
 
 
 def gcd(a: int, b: int) -> int:
