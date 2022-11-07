1217bbaa6297771728a1fc823d3bddbd314b12f5
857l <61349357+857l@users.noreply.github.com>
2022-11-07 11:46:26 +0300

    Update sudoku.py

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -4,6 +4,7 @@ from random import randint
 
 T = tp.TypeVar("T")
 
+
 def read_sudoku(path: tp.Union[str, pathlib.Path]) -> tp.List[tp.List[str]]:
     """Прочитать Судоку из указанного файла"""
     path = pathlib.Path(path)
